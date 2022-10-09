8289d1a625c8af6ea7c23eee7100ac40fc24991a
Edson <edsonservi@gmail.com>
2022-10-09 13:25:12 -0300

    Normalização do arquivo raiz!

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pyaudio
 import pyttsx3
 import acoes as act
 
+# CONFIGURAÇÕES DE PERSONALIZAÇÃO
 USUARIO = 'Edson'
 
-# CONFIGURA GLOBAIS E INICIA OS SERVIÇOS
+# CONFIGURA GLOBAIS E INICIO OS SERVIÇOS
 model = Model(r'F:\Programacao\Voskmodels\vosk-model-small-pt-0.3')
 voz = KaldiRecognizer(model, 16000)
 mic = pyaudio.PyAudio()
@@ -20,29 +21,44 @@ while True:
         texto = voz.Result()
         texto = texto[14:-3].lower()
         if texto != '':
-            texto = texto.replace(' assis', '')
-            print(f'Texto sem o assis: {texto}')
-            saudacao = ['bom dia', 'boa tarde', 'olá', 'oi', 'boa noite']
-            clima = ['temperatura', 'previsão do tempo', 'clima']
-            errado = ['veado', 'puta', 'caralho', 'cú', 'filha da puta', 'viado']
-            if texto in saudacao:
-                falar.say(act.saudacao(texto, USUARIO))
-                falar.runAndWait()
-            elif texto in clima:
-                falar.say(act.clima(USUARIO))
-                falar.runAndWait()
-            elif texto in errado:
-                falar.say(act.xingamento(USUARIO))
-                falar.runAndWait()
-            elif texto == 'música':
-                falar.say(f"Abrindo player de música,  {USUARIO}")
-                falar.runAndWait()
-                act.musica()
-            elif texto == 'fechar música':
-                act.fechar_musica()
-                falar.say(f"Pronto,  {USUARIO}, sem música!")
-                falar.runAndWait()
-            elif texto == 'desligar':
-                falar.say(f"Até mais, {USUARIO}, estou indo dormir!")
-                falar.runAndWait()
-                break
+            if 'assis' in texto:
+                texto = texto.replace(' assis', '')
+                texto = texto.replace('assis ', '')
+                testar = texto.split(' ')
+                # SAUDAÇÃO
+                if 'bom' in testar and 'dia' in testar \
+                        or 'boa' in testar and 'tarde' in testar \
+                        or 'boa' in testar and 'noite' in testar \
+                        or 'olá' in testar or 'oi' in testar:
+                    falar.say(act.saudacao(texto, USUARIO))
+                    falar.runAndWait()
+                # ABRIR MUSICA
+                elif 'música' in testar and 'tocar' in testar:
+                    falar.say(f"Abrindo player de música,  {USUARIO}")
+                    falar.runAndWait()
+                    act.musica()
+                # FECHAR MUSICA
+                elif 'música' in testar and 'fechar' in testar:
+                    act.fechar_musica()
+                    falar.say(f"Pronto,  {USUARIO}, sem música!")
+                    falar.runAndWait()
+                # CLIMA
+                elif 'temperatura' in testar:
+                    falar.say(act.clima(USUARIO))
+                    falar.runAndWait()
+                # PALAVRÕES
+                elif 'puta' in testar or \
+                        'viado' in testar or \
+                        'caralho' in testar or \
+                        'puta' in testar or \
+                        'cú' in testar or \
+                        'idiota' in testar or \
+                        'retardado' in testar or \
+                        'imbecil' in testar:
+                    falar.say(act.xingamento(USUARIO))
+                    falar.runAndWait()
+                # ENCERRAR
+                elif 'desligar' in testar:
+                    falar.say(f"Até mais, {USUARIO}, estou indo dormir!")
+                    falar.runAndWait()
+                    break
